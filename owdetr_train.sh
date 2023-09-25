8db7218453ac2ff266b58245f4206d9c8f340ce5
NickHezhuolin <1986013909@qq.com>
2023-09-25 13:09:56 +0000

    init from 100

--- owdetr_train.sh
+++ owdetr_train.sh
@@ -14,11 +14,20 @@ PY_ARGS=${@:1}
 #     ${PY_ARGS}
 
 # owdetr-train
+# python -u main_open_world.py \
+#     --output_dir ${EXP_DIR} --dataset owod --num_queries 100 --eval_every 1 \
+#     --PREV_INTRODUCED_CLS 0 --CUR_INTRODUCED_CLS 19 --data_root 'data/OWDETR' --train_set 't1_train' --test_set 't2_ft' --num_classes 81 \
+#     --unmatched_boxes --epochs 45 --top_unk 5 --featdim 1024 --NC_branch --nc_loss_coef 0.1 --nc_epoch 9 \
+#     --backbone 'dino_resnet50' \
+#     ${PY_ARGS}
+
+# owdetr-train
 python -u main_open_world.py \
     --output_dir ${EXP_DIR} --dataset owod --num_queries 100 --eval_every 5 \
-    --PREV_INTRODUCED_CLS 0 --CUR_INTRODUCED_CLS 19 --data_root 'data/OWDETR' --train_set 't1_train' --test_set 't2_ft' --num_classes 81 \
+    --PREV_INTRODUCED_CLS 0 --CUR_INTRODUCED_CLS 19 --data_root 'data/OWDETR' --train_set 't1_train' --test_set 'test' --num_classes 81 \
     --unmatched_boxes --epochs 45 --top_unk 5 --featdim 1024 --NC_branch --nc_loss_coef 0.1 --nc_epoch 9 \
     --backbone 'dino_resnet50' \
+    --resume 'exps/OWDETR_t1/checkpoint0004.pth' --eval \
     ${PY_ARGS}
 
 # owod-train
